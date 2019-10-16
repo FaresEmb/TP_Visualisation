d626cdaa162deed782ea3621bd9817ed2837b37b
EMB <fares.embouazza@laposte.net>
2019-10-16 12:23:37 +0200

    Merge branch 'master' of C:\Users\fares\Documents\IA-2019\Visu\TP with conflicts.

--- load_data.py
+++ load_data.py
@@ -1,7 +1,6 @@
 from zipfile import ZipFile
 import pandas as pd
 
-
 def get_files_list(zipfile = 'data/brut.zip'):
     with ZipFile(zipfile, 'r') as zip_obj:
         list_o_files = zip_obj.namelist()
@@ -10,23 +9,27 @@ def get_files_list(zipfile = 'data/brut.zip'):
             if elem != 'brut/':
                 list_files.append(elem)
     return list_files
-def valid_datetime(d):
-    try:
-        return parser.parse(d)
-    except:
-        return None
-def read_csv_from_zip(filename = None,zip_file='data/brut.zip',names=None):
+
+
+def read_csv_from_zip(filename = None,zip_file='data/brut.zip'):
     if filename == None:
         list_files = get_files_list()
         filename = list_files[0]
     zf = ZipFile(zip_file)
-    if names == None:
-        return pd.read_csv(zf.open(filename),sep=';')
-    else:
-        return pd.read_csv(zf.open(filename),sep=';',parse_dates=[names[0]],names=names)
+    return pd.read_csv(zf.open(filename),sep=';')
 
-#filename = get_files_list()[0]
-#df = read_csv_from_zip(filename=filename)
-#print(df.head)
+def read_csv_from_zip_date(filename = None,zip_file='data/brut.zip'):
+    if filename == None:
+        list_files = get_files_list()
+        filename = list_files[0]
+    zf = ZipFile(zip_file)
+    dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
+    data = pd.read_csv(zf.open(filename), sep=';',parse_dates=[1])
+    return data.take([0], axis=1)
+
+
+filename = get_files_list()[1]
+df = read_csv_from_zip_date(filename=filename)
+print(df.shape)
 
 print('......__ loading load_data module __ ......')
