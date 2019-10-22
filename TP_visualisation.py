a2bc7330b687381f160aa1e272ea8e0fe770c9c1
EMB <fares.embouazza@laposte.net>
2019-10-22 09:32:50 +0200

    Add sampling

--- TP_visualisation.py
+++ TP_visualisation.py
@@ -16,8 +16,15 @@ df_velo = ld.read_csv_from_zip(filename=velo_filename,names=names_velo)
 print((df_velo.date == None).value_counts())
 
 
-meteo_filename = ld.get_files_list()[1]
+meteo_filename = ld.get_files_list()[2]
 names_meteo = ['Timestamp','Status','Clouds','Humidity','Pressure','Rain','WindGust','WindVarEnd','WindVarBeg','WindDeg','WindSpeed','Snow','TemperatureMax','TemperatureMin','TemperatureTemp']
 df_meteo = ld.read_csv_from_zip(filename=meteo_filename,names=names_meteo)
 print((df_meteo.Timestamp == None).value_counts())
 
+def sampling(dataframe, time='10min', name='Timestamp'):
+    dataframe = dataframe.set_index('Timestamp').resample(time).last().dropna().reset_index()
+    return dataframe
+
+print(df_meteo.iloc[1:50,:])
+df_meteo = sampling(df_meteo)
+print(df_meteo.iloc[1:50,:])
--- load_data.py
+++ load_data.py
@@ -1,6 +1,7 @@
 from zipfile import ZipFile
 import pandas as pd
 
+
 def get_files_list(zipfile = 'data/brut.zip'):
     with ZipFile(zipfile, 'r') as zip_obj:
         list_o_files = zip_obj.namelist()
@@ -9,27 +10,23 @@ def get_files_list(zipfile = 'data/brut.zip'):
             if elem != 'brut/':
                 list_files.append(elem)
     return list_files
-
-
-def read_csv_from_zip(filename = None,zip_file='data/brut.zip'):
+def valid_datetime(d):
+    try:
+        return parser.parse(d)
+    except:
+        return None
+def read_csv_from_zip(filename = None,zip_file='data/brut.zip',names=None):
     if filename == None:
         list_files = get_files_list()
         filename = list_files[0]
     zf = ZipFile(zip_file)
-    return pd.read_csv(zf.open(filename),sep=';')
+    if names == None:
+        return pd.read_csv(zf.open(filename),sep=';')
+    else:
+        return pd.read_csv(zf.open(filename),sep=';',parse_dates=[names[0]],names=names)
 
-def read_csv_from_zip_date(filename = None,zip_file='data/brut.zip'):
-    if filename == None:
-        list_files = get_files_list()
-        filename = list_files[0]
-    zf = ZipFile(zip_file)
-    dateparse = lambda x: pd.datetime.strptime(x, '%Y-%m-%d %H:%M:%S')
-    data = pd.read_csv(zf.open(filename), sep=';',parse_dates=[1])
-    return data.take([0], axis=1)
-
-
-filename = get_files_list()[1]
-df = read_csv_from_zip_date(filename=filename)
-print(df.shape)
+#filename = get_files_list()[1]
+#df = read_csv_from_zip(filename=filename)
+#print(df.head())
 
 print('......__ loading load_data module __ ......')
